ada985681c4763d71de9d3498977624190aac39e
William Leung <gwilliamleung@gmail.com>
2022-06-27 23:59:08 -0400

    WIP DOM console game complete

--- script.js
+++ script.js
@@ -1,15 +1,84 @@
-const selectionButtons = document.querySelectorAll('[data-selection]')
+const userScore = 0;
+const computerScore = 0;
+const userScoreSpan = document.getElementById("userscore");
+const computerScoreSpan = document.getElementById("computerscore");
+const scoreBoardDiv = document.querySelector(".scoreboard");
+const resultDiv = document.querySelector(".result");
+const rockButton = document.getElementById("r");
+const paperButton = document.getElementById("p");
+const scissorsButton = document.getElementById("s");
 
-selectionButtons.forEach(selectionButton=>{
-    selectionButton.addEventListner('click',e=>{
-        const selectionName = selectionButton.dataset.selection
-        makeSelection(selectionName)
-    })
-})
-
-function makeSelection(selection){
-    console.log(selection)
+function getComputerChoice(){
+    const choices = ['r','p','s'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber]
 }
+
+function game(userChoice){
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice){
+        case "rs":
+        case "pr":
+        case "sp":
+            console.log("USER WINS");
+            console.log(computerChoice);
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            console.log("USER LOSES");
+            console.log(computerChoice);
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            console.log("TIE");
+            console.log(computerChoice);
+            break;
+    }
+
+}
+function main(){
+    rockButton.addEventListener('click',function(){
+        game("r");
+    })
+    paperButton.addEventListener('click',function(){
+        game("p");
+    })
+    scissorsButton.addEventListener('click',function(){
+        game("s");
+    })
+}
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const selectionButtons = document.querySelectorAll('[data-selection]')
+
+// selectionButtons.forEach(selectionButton=>{
+//     selectionButton.addEventListner('click',e=>{
+//         const selectionName = selectionButton.dataset.selection
+//         makeSelection(selectionName)
+//     })
+// })
+
+// function makeSelection(selection){
+//     console.log(selection)
+// }
 
 
 
