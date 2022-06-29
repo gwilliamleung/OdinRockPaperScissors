f3f912d0e4101351085ad21cb1f85bd18eb7a1ff
William Leung <gwilliamleung@gmail.com>
2022-06-29 18:54:16 -0400

    game ends on 5

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const resultP = document.querySelector(".result > p");
 const rockButton = document.getElementById("r");
 const paperButton = document.getElementById("p");
 const scissorsButton = document.getElementById("s");
+const message = document.getElementById("actionmessage");
 
 function getComputerChoice(){
     const choices = ['r','p','s'];
@@ -31,11 +32,28 @@ function lose(userChoice,computerChoice){
     computerScore++;
     userScoreSpan.innerHTML = userScore;
     computerScoreSpan.innerHTML = computerScore; 
-    resultP.innerHTML = `${convertToWord(userChoice)} beats ${convertToWord(computerChoice)}. You loses!`;
+    resultP.innerHTML = `${convertToWord(userChoice)} beats ${convertToWord(computerChoice)}. You lose!`;
 }
 
 function draw(userChoice,computerChoice){
     resultP.innerHTML = `${convertToWord(userChoice)} equals ${convertToWord(computerChoice)}. Its a draw!`;
+}
+
+function disableButtons() {
+    document.getElementById("r").disabled = true;
+    document.getElementById("p").disabled = true;
+    document.getElementById("s").disabled = true;
+}
+
+function testFor5 (){
+    if (userScore === 5 ){
+        message.innerHTML = `User wins congarts! Restart page to play again!`; 
+        disableButtons()        
+    }
+    if (computerScore === 5 ){
+        message.innerHTML = `Computer wins! Restart page to play again!`; 
+        disableButtons()
+    }
 }
 
 function game(userChoice){
@@ -45,30 +63,27 @@ function game(userChoice){
         case "pr":
         case "sp":
             win(userChoice, computerChoice);
+            testFor5();
             break;
         case "rp":
         case "ps":
         case "sr":
             lose(userChoice, computerChoice);
+            testFor5();
             break;
         case "rr":
         case "pp":
         case "ss":
             draw(userChoice, computerChoice);
+            testFor5();
             break;
     }
 
 }
 function main(){
-    rockButton.addEventListener('click',function(){
-        game("r");
-    })
-    paperButton.addEventListener('click',function(){
-        game("p");
-    })
-    scissorsButton.addEventListener('click',function(){
-        game("s");
-    })
+    rockButton.addEventListener('click', ()=> game("r"));
+    paperButton.addEventListener('click',()=>game("p"));
+    scissorsButton.addEventListener('click',()=>game("s"));
 }
 
 main()
